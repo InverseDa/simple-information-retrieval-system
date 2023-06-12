25906d4dcfe50d2c92bb58fca037449f94f3d75c
InverseDa <inverse_boom@outlook.com>
2023-06-12 23:34:41 +0800

    feat: update web spider

--- backend/python/web_scrapyer.py
+++ backend/python/web_scrapyer.py
@@ -3,7 +3,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options
 from configparser import ConfigParser
 from bs4 import BeautifulSoup
 # 导入环境变量
@@ -11,10 +11,10 @@ import os
 
 # 选项
 options = Options()
-# options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
-# options.add_argument('--no-sandbox')
-# options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 # 获取用户的主目录以及项目位置
 PATH = os.path.dirname(os.path.abspath(__file__)) + "/../"
@@ -29,32 +29,74 @@ password = config["Profile"]["password"]
 # 通过爬虫读取深圳大学
 url = r'https://xq40.szu.edu.cn/'
 # 定位到公文通的首页（先登录）
-browser = webdriver.Chrome(options=options)
+browser = webdriver.Edge(options=options)
 browser.get(url)
 
 browser.get(url + "/tzgg.htm")
-# 找到当前页面的所有文章table
-table = browser.find_element('xpath', '/html/body/div[4]/div/div[2]/ul')
-table_html = table.get_attribute('innerHTML')
-article_url = re.compile(
-    r'<a href="(?P<url>.*?)" class="wl">', re.S).finditer(table_html)
+
 # 找到当前页面的跳转按钮
 nav = browser.find_element(
     'xpath', '/html/body/div[4]/div/div[2]/div/div/span[1]')
 nav_html = nav.get_attribute('innerHTML')
 nav_url = re.compile(r'<a href="(?P<url>.*?)">', re.S).finditer(nav_html)
+
 for i, nav in enumerate(nav_url):
+    # 找到当前页面的所有文章table
+    table = browser.find_element('xpath', '/html/body/div[4]/div/div[2]/ul')
+    table_html = table.get_attribute('innerHTML')
+    article_url = re.compile(
+        r'<a href="(?P<url>.*?)" class="wl">', re.S).finditer(table_html)
     for j, article in enumerate(article_url):
         page_url = article.group('url')
         if re.compile(r'weixin').search(page_url):
             browser.get(page_url)
             # 找到文本主体
-            page_element = browser.find_element(
-                'xpath', '/html/body/div[1]/div[2]/div[1]/div/div[1]')
-            page_html = nav.get_attribute('innerHTML')
+            try:
+                page_element = browser.find_element(
+                    'xpath', '/html/body/div[1]/div[2]/div[1]/div/div[1]')
+            except Exception as e:
+                print(e)
+                continue
+            page_html = page_element.get_attribute('innerHTML')
+            soup = BeautifulSoup(page_html, 'html.parser')
             # 匹配所有中文
-            content = re.compile(r'[\u4e00-\u9fa5]+').findall(page_html)
-            ais = 1
+            content = re.sub(r'\n+', '\n', soup.get_text())
+        elif re.compile(r'bio').search(page_url):
+            browser.get(page_url)
+            # 找到文本主体
+            page_element = browser.find_element(
+                'xpath', '/html/body/div[1]/div[2]/article/div/div[1]/div[2]/div')
+            page_html = page_element.get_attribute('innerHTML')
+            soup = BeautifulSoup(page_html, 'html.parser')
+            # 匹配所有中文
+            content = re.sub(r'\n+', '\n', soup.get_text())
+        elif re.compile(r'ce').search(page_url):
+            browser.get(page_url)
+            # 找到文本主体
+            page_element = browser.find_element(
+                'xpath', '/html/body/div[7]/div/div[2]/form')
+            page_html = page_element.get_attribute('innerHTML')
+            soup = BeautifulSoup(page_html, 'html.parser')
+            # 匹配所有中文
+            content = re.sub(r'\n+', '\n', soup.get_text())
+        elif re.compile(r'info.lib.szu.edu.cn').search(page_url):
+            browser.get(page_url)
+            # 找到文本主体
+            page_element = browser.find_element(
+                'xpath', '/html/body/div[1]/div[2]/section[1]/main/div')
+            page_html = page_element.get_attribute('innerHTML')
+            soup = BeautifulSoup(page_html, 'html.parser')
+            # 匹配所有中文
+            content = re.sub(r'\n+', '\n', soup.get_text())
+        elif re.compile(r'info').search(page_url):
+            browser.get(url + page_url)
+            # 找到文本主体
+            page_element = browser.find_element(
+                'xpath', '/html/body/div[3]/div/div/div/div[1]/form/div/div[2]/div')
+            page_html = page_element.get_attribute('innerHTML')
+            soup = BeautifulSoup(page_html, 'html.parser')
+            # 匹配所有中文
+            content = re.sub(r'\n+', '\n', soup.get_text())
         else:
             browser.get(page_url)
             if j == 0:
@@ -65,6 +107,7 @@ for i, nav in enumerate(nav_url):
 
             content = browser.find_element(
                 'xpath', '/html/body/table/tbody/tr[2]/td/table/tbody/tr[3]/td/table/tbody/tr[2]/td')
+            content = content.text
 
         # 写入文件 /data/departmentName/title.txt
         file_path = os.path.join(PATH, "pages", f"page_{i * 10 + j}.txt")
@@ -72,7 +115,7 @@ for i, nav in enumerate(nav_url):
             print(f"File {file_path} already exists, skipping...")
         else:
             with open(file_path, 'w') as f:
-                f.write(content.text)
+                f.write(content)
 
     id = nav.group('url')
     browser.get(url + id)
